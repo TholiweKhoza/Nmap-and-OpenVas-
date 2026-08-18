540d2d1ef720f8e43c04d03eed743c4b91b85a05
tholiwe <tholiwekhoza@icloud.com>
2026-08-18 11:03:01 +0200

    declare missing variable in variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project_id" {
+  type    = string
+  default = "tholiwesproject"
+}
+
+variable "bucket_name" {
+  type    = string
+  default = "tholiwesproject-security-scans-bucket"
+}
+
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
+
+variable "zone" {
+  type    = string
+  default = "us-central1-a"
+}
